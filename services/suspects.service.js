2eefe0098f5ede7d4c462fef2f0ca6773fde1e96
Roni Cruz <ronidacruz@gmail.com>
2021-11-03 00:33:33 -0300

    Update suspects.service.js

--- services/suspects.service.js
+++ services/suspects.service.js
@@ -14,8 +14,8 @@ module.exports = {
 				{ name: "Cersei", passport: "GOT2000", photo: "https://shorturl.at/dAJY9" },
 				{ name: "Joker", passport: "BAT3000", photo: "https://shorturl.at/knozV" },
 				{ name: "Walter", passport: "BRE4000", photo: "https://shorturl.at/dmnpE" },
-				{ name: "Magneto", passport: "XMN5000", photo: "https://shorturl.at/kCKLZ" },
-				{ name: "Thanos", passport: "AVE6000", photo: "https://shorturl.at/qQTU9" },
+				{ name: "Magneto", passport: "XMN5000", photo: "https://shorturl.at/fzHNW" },
+				{ name: "Thanos", passport: "AVE6000", photo: "https://shorturl.at/motO4" },
 				{ name: "Arlequina", passport: "SSQ7000", photo: "https://shorturl.at/clB15" }
 			]);
 		}
